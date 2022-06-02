433c9423201038324d84f7ce63c728312f815c2e
jungmaan <chocoinhome@naver.com>
2022-06-03 01:34:35 +0900

    주석삭제

--- frontend/src/pages/Mainpage.js
+++ frontend/src/pages/Mainpage.js
@@ -32,16 +32,13 @@ function Mainpage() {
       <section className="section1">
         <div id="name_search">
               <div id="webname">
-                {/* 새로고침 <a href=“teamc_mainpage.html” target="_self"> */}
                   SEARCH.<span>IT</span>
-                  {/* </a> */}
                 </div>
             
               <div id="container">
                   <input type="search" placeholder="Search..."/> 
-                  {/* onChange={onChange}/>  */}
                   <button type="button" id="search_btn"/></div></div></section>
-                  {/* onClick={onClick}/> */}
+
                 
 
 
@@ -49,20 +46,12 @@ function Mainpage() {
 
       <section className="section2">
         <div id="nav_article">
-            {/* <BrowserRouter> */}
               <nav>
                   <ul>
                       <li id="recent"><Link to="recent">Recent</Link></li>
                       <li>&nbsp;&nbsp;</li><li>&nbsp;&nbsp;</li>
                       <li id="bag"><Link to="bag">What's In My Bag</Link></li></ul>
               </nav>    
-                {/* <article>
-                  <Routes>
-                    <Route path="recent" element={<Recent />} />
-                    <Route path="bag" element={<Bag />} />
-                  </Routes>
-                </article> */}
-            {/* </BrowserRouter> */}
         </div></section>
 
     </div>
