e9d3dc1433c59fffa9d17dc1161749467bfa0edd
teawon <xodnjsvkvk9@gmail.com>
2022-06-18 05:04:50 +0900

    chore : add searchProductList css , change pageCount

--- frontend/src/components/SearchProductList.js
+++ frontend/src/components/SearchProductList.js
@@ -1,19 +1,18 @@
 import React from "react";
 import {Link} from 'react-router-dom';
 import PropTypes from "prop-types";
-import "./Movie.css";
+import "./searchProductList.css";
 
 function SearchProductList({id, title, poster}) {
   return (
-    <div className="col-md-4">
-    <a href={"products/detail/"+id} target="_blank">
-      <img className="img-fluid"  className="img-thumbnail" src={poster} alt={title} titlt={title}></img>
-    <div className="product__data">
-      <h3 className="product__title">{
+    <div className="col-md-4 border-0">
+    <a className="link-dark product"
+     href={"products/detail/"+id} target="_blank">
+      <img className="img-fluid mx-auto" src={poster} alt={title} titlt={title}></img>
+      <h3 className="text-center">{
           title.replace(/<b>/gi,"").replace(/<\/b>/gi,"")
         }</h3>
 
-    </div>
   </a>
   </div>
   )
