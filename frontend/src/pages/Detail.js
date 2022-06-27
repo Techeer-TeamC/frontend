93657ea56b5e49f1d31977178ebdc04003b561d3
teawon <xodnjsvkvk9@gmail.com>
2022-06-28 03:05:20 +0900

    feat : Detail page

--- frontend/src/pages/Detail.js
+++ frontend/src/pages/Detail.js
@@ -1,14 +1,16 @@
 import axios from "axios";
 
 import React,  { useEffect, useState } from 'react';
-import { useParams , useNavigate} from 'react-router-dom';
-
+import { useParams , useNavigate} from 'react-router-dom'
+import './Detail.css';
 
 function Detail(props) {
     let { id } = useParams();
     const navigate = useNavigate();
     const[isLoading, setLoading] = useState(true);
     const[productData , setData] = useState({});
+
+
 
     useEffect( () => {
 
@@ -27,7 +29,7 @@ function Detail(props) {
 
 
         return (
-                <section className="container">
+                <section className="detail-container">
             {
                 isLoading
                     ? (<div className="loader">
@@ -36,21 +38,48 @@ function Detail(props) {
                     :  productData
                         ? (
                             <>
+                                <div className ="container">
+                                    <div className = "row">
+                                        <div className ="product_image col-md-6">
+                                            <img className="img-fluid" src="https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/mbp14-spacegray-select-202110_GEO_KR?wid=1808&hei=1680&fmt=jpeg&qlt=90&.v=1647363032344" alt="product-image"/>
+                                        </div>
+                                        <div className="product-detail col-md-6">
+                                            <div className="top-info">
+                                                <h2>{ productData.name }</h2>
+                                                <h3> 최저가 {productData.mallInfo[0].price}</h3>
+                                            </div>
+                                            <div className="mallInfo row justify-content-between">
+                                                <table className="table">
+                                                    <thead>
 
-                            <div className="post-view-row">
-                                <label>상품명</label>
-                                <label>{ productData.data.name }</label>
-                            </div>
-                            <div className="post-view-row">
-                                <label>가격</label>
-                                <label>{ productData.data.price }</label>
-                            </div>
+                                                    </thead>
+                                                    <tbody>
+
+                                                    <tr onClick={() => window.open(productData.mallInfo[0].name ,'_blank')}>
+                                                        <th>{ productData.mallInfo[0].name }</th>
+                                                        <th>{ productData.mallInfo[0].price }</th>
+                                                        <th>{ productData.mallInfo[0].delivery }</th>
+                                                        <th>{ productData.mallInfo[0].paymentOption }</th>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>{ productData.mallInfo[0].name }</th>
+                                                        <th>{ productData.mallInfo[0].price }</th>
+                                                        <th>{ productData.mallInfo[0].delivery }</th>
+                                                        <th>{ productData.mallInfo[0].paymentOption }</th>
+                                                    </tr>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
 
 
                         </>
                     ) : '해당 게시글을 찾을 수 없습니다.'
             }
-            <button className="post-view-go-list-btn" onClick={() => navigate('/search')}>>목록으로 돌아가기</button>
+
     </section>);
 
 
