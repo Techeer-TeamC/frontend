76d9b0c2742d82936f917cbe5273f670ed707acf
teawon <xodnjsvkvk9@gmail.com>
2022-06-29 17:57:26 +0900

    chore : rebase

--- frontend/src/assets/auth.js
+++ frontend/src/assets/auth.js
@@ -10,4 +10,35 @@
 
 //     }
 
-//로그인 완료시 버튼 문구 수정/ 라우터 설정/ 
+const onClickLogin = (e) => {
+    //토큰만료시 처리 잘 되나 확인 중
+    fetch('http://localhost:3000/assets/mockData.json', {
+        // method: 'POST',
+    //     headers: {
+    //       'Authorization': 'Bearer '+ actoken,
+    //   },
+    //     body: JSON.stringify({
+    //     'email': inputId,
+    //     'password': inputPw
+    //     })
+    })
+
+    .then(response => response.json())
+    .then(response => {
+    if (!response.success){
+        if (response.code === "J004"){
+            console.log("access토큰 만료")
+           // {GetAccessToken}
+        }
+        if (response.code === "J008"){
+            console.log("refresh토큰 만료")
+         //   {GetRefreshToken}
+        }
+    }
+    })
+}
+return(
+    <div>
+    <button type='button' onClick={onClickLogin}>Login</button> </div>
+);}    
+export default Auth;
