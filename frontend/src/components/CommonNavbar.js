c3d97926f44ca46c3350417244ff8707ea1a7c75
teawon <xodnjsvkvk9@gmail.com>
2022-06-29 21:39:37 +0900

    chore : add css Navbar

--- frontend/src/components/CommonNavbar.js
+++ frontend/src/components/CommonNavbar.js
@@ -1,50 +1,38 @@
-import React from "react";
-import {Link} from 'react-router-dom';
-import PropTypes from "prop-types";
-
+import React, {useState} from "react";
+import { Nav, Navbar, NavItem, NavDropdown, Container} from "react-bootstrap";
+import { Link } from "react-router-dom";
+import imgLogo from '../assets/logo.png';
 function CommonNavbar() {
-
+  
+  const [isAuthentication, setAuthentication] = useState(false);
+  
   return (
       <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">Navbar</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse"
-                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Link</a>
-            </li>
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Dropdown
-              </a>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a className="dropdown-item" href="#">Action</a>
-                <a className="dropdown-item" href="#">Another action</a>
-                <div className="dropdown-divider"></div>
-                <a className="dropdown-item" href="#">Something else here</a>
-              </div>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link disabled" href="#">Disabled</a>
-            </li>
-          </ul>
-          <form className="form-inline my-2 my-lg-0">
-            <input className="form-control mr-sm-2" type="search" placeholder="Search"
-                   aria-label="Search"/>
-              <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
-        </div>
-      </nav>
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Brand href="/"><img className="img-fluid" src={imgLogo}/> </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+              </Nav>
+              <Nav>
+                {isAuthentication ?(
+                    <NavDropdown title="회원정보" id="basic-nav-dropdown">
+                      <NavDropdown.Item href="#action/3.1">회원 정보</NavDropdown.Item>
+                      <NavDropdown.Item href="#action/3.2">로그아웃</NavDropdown.Item>
+                      <NavDropdown.Divider />
+                      <NavDropdown.Item href="#action/3.4">등록한 알림 설정</NavDropdown.Item>
+                    </NavDropdown>
+                ) : ( 
+                      <>
+                        <Nav.Link href="/">Login</Nav.Link>
+                        <Nav.Link href="#link">Signup</Nav.Link>
+                        </>)}
+                
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
       </div>
   )
 };
