ca38b8ee63db11f1c4fe8daf9ae942a6edc0e37d
teawon <xodnjsvkvk9@gmail.com>
2022-07-10 13:15:37 +0900

    chore : get ChartData by using apiRequest

--- frontend/src/components/Graph.js
+++ frontend/src/components/Graph.js
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 
@@ -10,105 +11,16 @@ import HighchartsReact from "highcharts-react-official";
 
 function Graph(){
 
-  const option2 = {
-    chart: {
-      type: 'spline',
-      animation: Highcharts.svg, // don't animate in old IE
-      marginRight: 10,
-      events: {
-        load: function () {
-          // set up the updating of the chart each second
-          var series = this.series[0];
-          setInterval(function () {
-            var x = (new Date()).getTime(), // current time
-                y = Math.random();
-            series.addPoint([x, y], true, true);
-          }, 1000);
-        }
-      }
-    },
-
-    time: {
-      useUTC: false
-    },
-
-    title: {
-      text: 'Live random data'
-    },
-
-    accessibility: {
-      announceNewData: {
-        enabled: true,
-        minAnnounceInterval: 15000,
-        announcementFormatter: function (allSeries, newSeries, newPoint) {
-          if (newPoint) {
-            return 'New point added. Value: ' + newPoint.y;
-          }
-          return false;
-        }
-      }
-    },
-
-    xAxis: {
-      type: 'datetime',
-      tickPixelInterval: 150
-    },
-
-    yAxis: {
-      title: {
-        text: 'Value'
-      },
-      plotLines: [{
-        value: 0,
-        width: 1,
-        color: '#808080'
-      }]
-    },
-
-    tooltip: {
-      headerFormat: '<b>{series.name}</b><br/>',
-      pointFormat: '{point.x:%Y-%m-%d %H:%M:%S}<br/>{point.y:.2f}'
-    },
-
-    legend: {
-      enabled: false
-    },
-
-    exporting: {
-      enabled: false
-    },
-
-    series: [{
-      name: 'Random data',
-      data: (function () {
-        // generate an array of random data
-        var data = [],
-            time = (new Date()).getTime(),
-            i;
-
-        for (i = -19; i <= 0; i += 1) {
-          data.push({
-            x: time + i * 1000,
-            y: Math.random()
-          });
-        }
-        return data;
-      }())
-    }]
-
-
-  };
-
-
-
-  const option1 = {
+  
+  const [chart, setChart] = useState({
     title: {
       text: '최저가 추이 그래프'
     },
 
-    // subtitle: {
-    //   text: '최저가 추이 그래프'
-    // },
+    subtitle: {
+      text: '(30분)'
+    },
+    
 
     yAxis: {
       title: {
@@ -133,23 +45,25 @@ function Graph(){
         label: {
           connectorAllowed: false
         },
-        pointStart: 2020
+        pointStart: 2000
+
       }
     },
 
     series: [{
-      name: '최저가',
+      name: "asd",
       data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
     }, {
-      name: '쿠팡',
+      name: 'bcd',
       data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
     }, {
-      name: '옥션',
+      name: 'efg',
       data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
     }, {
-      name: '11번가',
+      name: 'asdasd',
       data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
     }],
+    series: [],
 
     responsive: {
       rules: [{
@@ -165,25 +79,59 @@ function Graph(){
         }
       }]
     }
+  });
 
-  };
   
-  const [chart, setChart] = useState(option1);
+  
+  
+  
+  useEffect( () => {
+    const fetchData = async () => {
+      const result = await axios(
+          `https://ea27c19b-8e3f-4b60-a355-e4ef6cd5a1ee.mock.pstmn.io//chart`
+      )
+        .then(function (response) {
+          response= response.data.data;
+          console.log("성공");
+          console.log(response);
+          setChart({
+
+            series: [{
+              name: response[0].mallName,
+              data: response[0].price
+            }, {
+              name: response[1].mallName,
+              data: response[1].price
+            }, {
+              name: response[2].mallName,
+              data: response[2].price
+            }
+          ]
+        });
+
+
+        })
+        .catch(function (error) {
+          console.log("에러");
+        })
+    }
+    fetchData();
+  },[]);
   
   return(
       <div>
-        <ui>
-          <li>
-            <a onClick={() => setChart(option1) }>
-              <span>1번 차트</span>
-            </a>
-          </li>
-          <li>
-            <a onClick={() => setChart(option2) }>
-              <span> 2번 차트</span>
-            </a>
-          </li>
-        </ui>
+        {/*<ui>*/}
+        {/*  <li>*/}
+        {/*    <a onClick={() => setChart("1") }>*/}
+        {/*      <span>1번 차트</span>*/}
+        {/*    </a>*/}
+        {/*  </li>*/}
+        {/*  <li>*/}
+        {/*    <a onClick={() => setChart(option2) }>*/}
+        {/*      <span> 2번 차트</span>*/}
+        {/*    </a>*/}
+        {/*  </li>*/}
+        {/*</ui>*/}
         <HighchartsReact
             containerProps={{ className: "home_body-chart" }}
             highcharts={Highcharts}
